5222dc96b21d5a3d5544d58f0bfe8cfdd6a55184
satya-tyagi <121234682+satya-tyagi@users.noreply.github.com>
2023-10-17 03:34:21 -1200

    Delete mail_server-master directory

--- mail_server-master/controller/test.controller.js
+++
@@ -1,5 +0,0 @@
-
-
-exports.testServer = async (req, resp) => {
-    resp.status(200).json({ status: "true", msg: "Server Started" });
-};
--- mail_server-master/model/mail.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-const mailSchema=mongoose.Schema({
-    reciepents:[String],
-    subject:String,
-    msg:String
-});
-module.exports=mongoose.model('mail',mailSchema);
